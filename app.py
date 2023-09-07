23f30daf3fecb3e1c659fb6aaee1841e17d3746b
Luis Adames <adames1601@gmail.com>
2023-09-07 01:16:56 -0400

    cambios

--- app.py
+++ app.py
@@ -1,25 +1,57 @@
+import time
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+import board
+import adafruit_rfm9x
+import socketio
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# Configurar RFM9x LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+try:
+    rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RESET, 868.2)
+    print('RFM9x detected')
+except RuntimeError:
+    print('RFM9x error')
+
+# Rutas para la página web
 @app.route('/')
 def index():
-    return render_template('index.html')  # Crea un archivo HTML para mostrar los datos en la página web
+    return render_template('index.html')
 
+# Eventos de Socket.IO
 @socketio.on('connect')
 def handle_connect():
     print('Cliente conectado')
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    print('Cliente desconectado')
+    print('Cliente desconectado') 
 
 @socketio.on('send_data')
 def handle_data(data):
     print('Datos recibidos:', data)
-    emit('update_data', data, broadcast=True)  # Emitir datos a todos los clientes conectados
+    emit('update_data', data, broadcast=True)
 
+# Bucle para recibir datos LoRa y enviar al servidor WebSocket
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    while True:
+        packet = rfm9x.receive()
+        if packet:
+            temp = int.from_bytes(packet[2:4], byteorder='little') / 10.0
+            humd = int.from_bytes(packet[4:6], byteorder='little') / 10.0
+
+            print("Received temperature:", temp, "C")
+            print("Received humidity:", humd, "%")
+            
+            socketio.emit('send_data', {'temp': temp, 'humd': humd})
+            
+        time.sleep(1)
