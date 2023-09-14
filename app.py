f54b71afea4b647f8e37ea0bc2e2fb8b10a7e036
Luis Adames <adames1601@gmail.com>
2023-09-13 20:37:08 -0400

    vamos a ver

--- app.py
+++ app.py
@@ -55,9 +55,8 @@ def receive_data():
 
 if __name__ == '__main__':
     # Iniciar el hilo para recopilar y emitir datos
-    #data_thread = threading.Thread(target=receive_data)
-    #data_thread.daemon = True
-   # data_thread.start()
-    receive_data()
+    data_thread = threading.Thread(target=receive_data)
+    data_thread.daemon = True
+    data_thread.start()
     # Ejecutar el servidor Flask en el hilo principal
     socketio.run(app, host='0.0.0.0', port=3000, debug=False)
