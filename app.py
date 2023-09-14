00bd4c3539d8e6f3cac8821fa946efa70594c7cf
Luis Adames <adames1601@gmail.com>
2023-09-13 20:16:30 -0400

    peuab2

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def receive_data():
             socketio.emit('temp', temp)  
             socketio.emit('humd', humd)
             
-        time.sleep(6)
+        time.sleep(4)
 
 if __name__ == '__main__':
     # Iniciar el hilo para recopilar y emitir datos
